c2d72ac056a3434c665cc3c114fddb3119316ce7
AnxiousAnt <92052531+AnxiousAnt@users.noreply.github.com>
2023-09-19 23:02:20 +0530

    call the chatGPT API

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ import openai
 import os
 
 os.environ['OPENAI_API_KEY'] = ''
-
+openai.api_key = ''
 #return everything that the user inputs.
 
 @cl.on_message
 async def main(message: str):
-    await cl.Message(content = message).send()
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = [
+            {"role": "assistant","content":"you are a helpful assistant"},
+            {"role": "user", "content": message}
+        ],
+        temperature = 1,
+    )
+
+
+    await cl.Message(content = str(response)).send()
